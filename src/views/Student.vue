c5b8adefb219d3ec89f102d4f8e0e19e9f09930b
Yolo-Lu <842105661@qq.com>
2019-10-17 17:48:33 +0800

    新增文件

---
+++ src/views/Student.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    Student
+  </div>
+  
+</template>
+
+<script>
+  export default {
+    name: "Student",
+    components: {},
+    props: {},
+    data() {
+      return {}
+    },
+    methods: {},
+
+    mounted() {
+
+    },
+    created() {
+
+    },
+    filters: {},
+    computed: {},
+    watch: {},
+    directives: {}
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
