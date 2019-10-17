deb69969ad07a740b0fa02517e0975b0505459ba
Yolo-Lu <842105661@qq.com>
2019-10-17 17:46:35 +0800

    first commit

---
+++ src/components/Child-slot.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    子组件的内容
+    <div><slot></slot></div>
+    <div style="color: red"><slot name="ss">依旧是子组件</slot></div>
+    <div><slot name="aa"></slot></div>
+  </div>
+  
+</template>
+
+<script>
+  export default {
+    name: "Child-slot",
+    components: {},
+    props: {},
+    data() {
+      return {}
+    },
+    methods: {},
+
+    mounted() {
+
+    },
+    created() {
+
+    },
+    filters: {},
+    computed: {},
+    watch: {},
+    directives: {}
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ChildSlot>插入的内容
+      <div slot="aa">我要插入到子组件</div>
+    </ChildSlot>
+  </div>
+
+</template>
+
+<script>
+  import ChildSlot from '../components/Child-slot'
+  export default {
+    name: "AboutSlot",
+    components: {
+      ChildSlot
+    },
+    props: {},
+    data() {
+      return {}
+    },
+    methods: {},
+
+    mounted() {
+
+    },
+    created() {
+
+    },
+    filters: {},
+    computed: {},
+    watch: {},
+    directives: {}
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
